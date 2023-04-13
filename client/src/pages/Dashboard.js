9a085ccba68dcbaa005c460d3e596c8ec5d0192f
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2023-04-13 11:24:49 +0545

    Client and server Connected

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Dashboard = () => {
+
+  const fetchData = async () =>{
+
+      try {
+        const response = await fetch("/api/v1")
+        const data = await response.json()
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+  }
+
+  useEffect(()=>{
+    fetchData()
+  },[])
   return (
     <div>
         <h1>Dashboard</h1>
